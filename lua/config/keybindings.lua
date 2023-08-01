605c95e877d7849f50f7401efca93f2e54e31fbe
Gabriel Lisboa <gabriellisboa.rx@gmail.com>
2023-08-01 19:51:53 -0300

    Add goto

--- lua/config/keybindings.lua
+++ lua/config/keybindings.lua
@@ -51,3 +51,16 @@ map_key("n", "<C-l>", "<C-W>l")
 map_key("n", "<leader>j", ":bp<CR>")
 map_key("n", "<leader>l", ":bn<CR>")
 
+-- GoTO
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function (ev)
+    local opts = { buffer = ev.buf }
+
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+  end
+})
+
