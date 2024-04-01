24b276401f2376a3418edf1e369706d68cbe1485
Gabriel Lisboa <gabriellisboa.rx@gmail.com>
2024-04-01 11:29:19 -0300

    Update better-vim.lua

--- better-vim.lua
+++ better-vim.lua
@@ -1,29 +1,39 @@
 return {
-    dashboard = {
-      header = {
-        "██╗     ██╗███████╗██████╗  ██████╗  █████╗ ",
-        "██║     ██║██╔════╝██╔══██╗██╔═══██╗██╔══██╗",
-        "██║     ██║███████╗██████╔╝██║   ██║███████║",
-        "██║     ██║╚════██║██╔══██╗██║   ██║██╔══██║",
-        "███████╗██║███████║██████╔╝╚██████╔╝██║  ██║",
-        "╚══════╝╚═╝╚══════╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝",
-        "",
-        "",
-      }
-    },
-    theme = {
-      name = "catppuccin",
-      catppuccin_flavour = "frappe"
-    },
-    flags = {
-      format_on_save = true
-    },
-    mappings = {
-      by_mode = {
-        n = { -- Normal mode mappings
-          ["<leader>th"] = { ":split <cr>:terminal <cr>", "Open terminal horizontally" },
-          ["<leader>tv"] = { ":vsplit <cr>:terminal <cr>", "Open terminal vertically" },
-        },
-      }
+  dashboard = {
+    header = {
+      "██╗     ██╗███████╗██████╗  ██████╗  █████╗ ",
+      "██║     ██║██╔════╝██╔══██╗██╔═══██╗██╔══██╗",
+      "██║     ██║███████╗██████╔╝██║   ██║███████║",
+      "██║     ██║╚════██║██╔══██╗██║   ██║██╔══██║",
+      "███████╗██║███████║██████╔╝╚██████╔╝██║  ██║",
+      "╚══════╝╚═╝╚══════╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝",
+      "",
+      "",
     }
-  }
+  },
+  theme = {
+    name = "catppuccin",
+    catppuccin_flavour = "frappe"
+  },
+  flags = {
+    format_on_save = true
+  },
+  mappings = {
+    by_mode = {
+      n = { -- Normal mode mappings
+        ["<leader>th"] = { ":split <cr>:terminal <cr>", "Open terminal horizontally" },
+        ["<leader>tv"] = { ":vsplit <cr>:terminal <cr>", "Open terminal vertically" },
+      },
+    }
+  },
+  lsps = {
+    clangd = {
+      settings = {
+        cmd = { "clangd -I/usr/local" }
+      }
+    },
+    gopls = {
+
+    }
+  },
+}
