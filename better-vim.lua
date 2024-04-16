0adf16a0cdf68caad0737754eacd5a5d1adee00f
Gabriel Lisboa <gabriellisboa.rx@gmail.com>
2024-04-16 18:17:53 -0300

    Update better-vim.lua

--- better-vim.lua
+++ better-vim.lua
@@ -15,7 +15,7 @@ return {
     after_setup = function()
       vim.opt.foldmethod = 'expr'
       vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
-      vim.opt.foldcolumn = '1'   -- '0' is not bad
+      vim.opt.foldcolumn = '1'   -- '1' is not bad
       vim.opt.foldlevel = 99     -- Using ufo provider need a large value, feel free to decrease the value
       vim.opt.foldlevelstart = 1
       vim.opt.foldenable = false -- if this option is true and fold method option is other than normal, every time a document is opened everything will be folded.
@@ -25,8 +25,11 @@ return {
     messages = {
       view = "mini",         -- default view for messages
       view_error = "notify", -- view for errors
-      view_warn = "notify",  -- view for warnings
+      view_warn = "mini",    -- view for warnings
     },
+    notify = {
+      view = "mini"
+    }
   },
   plugins = {
     "arzg/vim-colors-xcode",
@@ -34,6 +37,9 @@ return {
     {
       "Civitasv/cmake-tools.nvim",
       dependencies = "nvim-lua/plenary.nvim",
+    },
+    {
+      "tpope/vim-fugitive",
     },
   },
   theme = {
@@ -47,7 +53,18 @@ return {
       n = { -- Normal mode mappings
         ["<leader>th"] = { ":split <cr>:terminal <cr>", "Open terminal horizontally" },
         ["<leader>tv"] = { ":vsplit <cr>:terminal <cr>", "Open terminal vertically" },
+        ["<tab>"] = { ">>", "Move line right" },
+        ["<S-tab>"] = { "<<", "Move line left" },
+        ["<M-j>"] = { "mz:m+<cr>`z", "Move lines down" },
+        ["<M-k>"] = { "mz:m-2<cr>`z", "Move lines up" },
+        ["<leader>gs"] = { ":Git<cr>", "Show Git changes" },
+        ["<leader>gf"] = { ":Gvdiffsplit<cr>", "Show Git diff"},
       },
+      v = {
+        ["<tab>"] = { ">gv", "Move a group of lines right" },
+        ["<S-tab>"] = { "<gv", "Move a group of lines left" },
+        ["<M-j>"] = { ":m'>+<cr>`<my`>mzgv`yo`z", "Move lines down" },
+        ["<M-k>"] = { ":m'<-2<cr>`>my`<mzgv`yo`z", "Move lines up" },     }
     }
   },
   lsps = {
