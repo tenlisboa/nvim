66dd4b8cd1c74aea70a73fc99058eb09725d245f
Gabriel Lisboa <gabriellisboa.rx@gmail.com>
2024-08-10 09:52:00 -0300

    install dap usign mason dap adapters

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,7 +1,18 @@
 return {
-	"mfussenegger/nvim-dap",
+	"jay-babu/mason-nvim-dap.nvim",
+	dependencies = { "mfussenegger/nvim-dap" },
+	requires = { "williamboman/mason.nvim" },
 	config = function()
 		vim.fn.sign_define('DapBreakpoint', { text = '●', texthl = '', linehl = '', numhl = '' })
 		vim.fn.sign_define('DapStopped', { text = '▶️', texthl = '', linehl = '', numhl = '' })
+
+		require("mason-nvim-dap").setup({
+			ensure_installed = { "delve" },
+			handlers = {
+				function(config)
+					require("mason-nvim-dap").default_setup(config)
+				end,
+			}
+		})
 	end
 }
