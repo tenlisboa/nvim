a686e8e81f069a4b51daddb13cb741445d1599fe
Gabriel Lisboa <gabriel.lisboa@mevosaude.com.br>
2024-09-16 16:48:50 -0300

    fix: broken lsp on update from tsserver to ts_ls

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -4,7 +4,23 @@ return {
   config = function()
     require("mason").setup()
     require("mason-lspconfig").setup({
-      ensure_installed = { "lua_ls", "gopls", "ts_ls", "eslint", "jsonls", "tailwindcss" }
+      ensure_installed = { "lua_ls", "gopls", "tsserver", "eslint", "jsonls", "tailwindcss", "jsonls" }
     })
+    require("mason-lspconfig").setup_handlers({
+				-- Will be called for each installed server that doesn't have
+				-- a dedicated handler.
+				--
+				function(server_name) -- default handler (optional)
+					-- https://github.com/neovim/nvim-lspconfig/pull/3232
+					if server_name == "tsserver" then
+						server_name = "ts_ls"
+					end
+					local capabilities = require("cmp_nvim_lsp").default_capabilities()
+					require("lspconfig")[server_name].setup({
+
+						capabilities = capabilities,
+					})
+				end,
+			})
   end
 }
