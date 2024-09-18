d702c21a80874f303e59dda90d112a0ff31469c5
Gabriel Lisboa <gabriellisboa.rx@gmail.com>
2024-09-17 22:13:21 -0300

    fix: lsp error

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,9 +2,15 @@ return {
   "williamboman/mason.nvim",
   dependencies = { "williamboman/mason-lspconfig.nvim", "neovim/nvim-lspconfig", "hrsh7th/cmp-nvim-lsp" },
   config = function()
+    local lspconfig = require("lspconfig")
     require("mason").setup()
     require("mason-lspconfig").setup({
-      ensure_installed = { "lua_ls", "eslint", "jsonls", "tailwindcss", "phpactor" }
+      ensure_installed = { "lua_ls", "eslint", "jsonls", "tailwindcss", "phpactor", "ts_ls" },
+      handlers = {
+        function(server)
+          lspconfig[server].setup({})
+        end
+      }
     })
   end
 }
