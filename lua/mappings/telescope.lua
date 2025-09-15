66762644f9085128ee10980791f8329fea6373c5
Gabriel Lisboa <gabriellisboa.rx@gmail.com>
2025-09-14 22:28:23 -0300

    update telescope configuration and add file browser extension

--- lua/mappings/telescope.lua
+++ lua/mappings/telescope.lua
@@ -1,14 +1,14 @@
 local map_keys = require("utils.mapper")
-local tl_actions = require("telescope.actions")
-local tl_builtins = require("telescope.builtin")
 
 map_keys({
   n = {
-    ["-"] = ":lua require('telescope.builtin').find_files()<CR>",
-    ["<leader>fg"] = ":lua require('telescope.builtin').live_grep()<CR>",
-    ["<leader>fb"] = ":lua require('telescope.builtin').buffers()<CR>",
+    ["-"] = "<cmd>Telescope file_browser<CR>",
+    ["<leader>ff"] = "<cmd>Telescope find_files<CR>",
+    ["<leader>fg"] = "<cmd>Telescope live_grep<CR>",
+    ["<leader>fb"] = "<cmd>Telescope buffers<CR>",
   },
   i = {
-    ["<C-q>"] = ":lua require('telescope.actions').send_selected_to_qflist()<CR>:lua require('telescope.actions').open_qflist()<CR>"  -- send selected to quickfixlist
+    ["<C-q>"] =
+    "<cmd>lua require('telescope.actions').send_selected_to_qflist()<CR><cmd>lua require('telescope.actions').open_qflist()<CR>" -- send selected to quickfixlist
   },
 })
--- lua/plugins/file_finder.lua
+++ lua/plugins/file_finder.lua
@@ -3,18 +3,49 @@ return {
   branch = "0.1.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' }
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' },
+    "nvim-telescope/telescope-file-browser.nvim"
   },
   cmd = "Telescope",
   config = function()
     local tl = require('telescope')
-    local tl_actions = require('telescope.actions')
 
     tl.setup({
       defaults = {
         file_ignore_patterns = { "node_modules", ".git/", "vendor" },
+        hidden = true,
+        respect_gitignore = false,
       },
+      pickers = {
+        find_files = {
+          theme = "dropdown",
+          previewer = false,
+          hidden = true,
+          no_ignore = false,
+          follow = true,
+        }
+      },
+      extensions = {
+        file_browser = {
+          theme = "ivy",
+          hidden = true,
+          respect_gitignore = false,
+          grouped = true,
+          select_buffer = true,
+          initial_mode = "normal",
+          mappings = {
+            ["n"] = {
+              ["N"] = tl.extensions.file_browser.actions.create,
+              ["h"] = tl.extensions.file_browser.actions.goto_parent_dir,
+              ["l"] = tl.extensions.file_browser.actions.change_cwd,
+              ["/"] = function() vim.cmd('startinsert') end,
+            },
+          }
+        }
+      }
     })
-    
+
+    tl.load_extension('fzf')
+    tl.load_extension('file_browser')
   end
 }
