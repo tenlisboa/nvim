5434aabd0828fbe390a7ca94415d486adce71185
Gabriel Lisboa <gabriellisboa.rx@gmail.com>
2025-09-15 09:28:34 -0300

    fix: mapping to move between splits

--- lua/mappings/default.lua
+++ lua/mappings/default.lua
@@ -4,9 +4,9 @@ vim.cmd(":tnoremap <Esc> <c-\\><c-n>")
 
 map_keys({
   n = {
-    ["<c-k>"] = ":wincmd k<cr>",
-    ["<c-j>"] = ":wincmd j<cr>",
-    ["<c-h>"] = ":wincmd h<cr>",
-    ["<c-l>"] = ":wincmd l<cr>",
+    ["<c-k>"] = "<c-w>k",
+    ["<c-j>"] = "<c-w>j",
+    ["<c-h>"] = "<c-w>h",
+    ["<c-l>"] = "<c-w>l",
   },
 })
